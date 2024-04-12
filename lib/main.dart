091024bf0fa1b272cbb40570ee00b259a6854d86
y-motoba@gmail.com <y-motoba@rvsta.co.jp>
2024-04-12 14:12:34 +0900

    dart-define-from-fileの導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_github_search/presentation/router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
+import 'common/logger.dart';
 import 'di.dart';
 
-void main() {
+Future<void> debugLog() async {
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+  String appName = packageInfo.appName;
+  String packageName = packageInfo.packageName;
+  String version = packageInfo.version;
+  String buildNumber = packageInfo.buildNumber;
+  logger.d('$appName $packageName $version $buildNumber');
+  logger.d(const String.fromEnvironment('appName'));
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  debugLog();
   runApp(
     ProviderScope(
       overrides: overrides,
